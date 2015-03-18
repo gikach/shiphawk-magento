9eb2d4060ae690d1531c00dd8ad55858f411d927
gikach@gmail.com <gikach@gmail.com>
2015-03-18 12:18:47 +0200

    SHIP-26: add origin email product attribute, move Origin Address 2 right after Origin Addres

--- app/code/community/Shiphawk/Shipping/sql/shiphawk_setup/mysql4-install-0.1.0.php
+++ app/code/community/Shiphawk/Shipping/sql/shiphawk_setup/mysql4-install-0.1.0.php
@@ -165,6 +165,20 @@ $address_line_1_origin_data = array (
 
 $installer->addAttribute('catalog_product','shiphawk_origin_addressline1',$address_line_1_origin_data);
 
+$address_line_2_origin_data = array (
+    'attribute_set' =>  'Default',
+    'group' => 'ShipHawk Attributes',
+    'label'    => 'Origin Address 2',
+    'visible'     => true,
+    'type'     => 'varchar',
+    'input'    => 'text',
+    'system'   => false,
+    'required' => false,
+    'user_defined' => 1,
+);
+
+$installer->addAttribute('catalog_product','shiphawk_origin_addressline2',$address_line_2_origin_data);
+
 $city_origin_data = array (
     'attribute_set' =>  'Default',
     'group' => 'ShipHawk Attributes',
